043c3cb5d6eed0499d9212d0b6ba4176fd0949c4
hamdi <hamdiyyildirim@gmail.com>
2024-06-02 22:36:25 +0200

    click and check url

--- src/test/java/FirstUITest.java
+++ src/test/java/FirstUITest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -26,6 +27,32 @@ public class FirstUITest {
 
         //Assert
         Assertions.assertEquals(user, actualUserName);
+
+        driver.close();
+    }
+
+    @Test
+    void repoLinkGoesToCorrectRepo() throws InterruptedException {
+        //Arrange
+        System.setProperty("webdriver.chrome.driver", "/Users/hyildirim/Documents/informatique/project/testing/chromedriver");
+        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String user = "andrejs-ps";
+        driver.get("https://github.com/" + user);
+
+        //Act
+        String repo = "automated-tests-in-java-with-fluent-interface-using-webdriver-selenium";
+        WebElement repoLink = driver.findElement(By.linkText(repo));
+        repoLink.click();
+
+        Thread.sleep(2000);
+        String actualUrl = driver.getCurrentUrl();
+
+        //Assert
+        Assertions.assertEquals("https://github.com/andrejs-ps/" + repo, actualUrl);
 
         driver.close();
     }
