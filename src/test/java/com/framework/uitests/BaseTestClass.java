73304c0dfba8248a3ee1ddacae3fbbbf4994b019
hamdi <hamdiyyildirim@gmail.com>
2024-06-04 10:49:12 +0200

    Moving support code to main

--- src/test/java/com/framework/uitests/BaseTestClass.java
+++ src/test/java/com/framework/uitests/BaseTestClass.java
@@ -1,17 +1,9 @@
 package com.framework.uitests;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import static com.testframework.DriverFactory.getChromeDriver;
 
 public class BaseTestClass {
     public static final String BASE_URL = "https://github.com/";
@@ -19,12 +11,10 @@ public class BaseTestClass {
 
     @BeforeAll
     static void setup() {
-        System.setProperty("webdriver.chrome.driver", "/Users/hyildirim/Documents/informatique/project/testing/chromedriver");
-        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
-
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver = getChromeDriver();
     }
+
+
 
     @AfterAll
     static void cleanUp() {
