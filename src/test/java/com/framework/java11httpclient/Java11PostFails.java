0aef5161c25d729009798a40fb62b45b12ef939e
hamdi <hamdiyyildirim@gmail.com>
2024-06-06 00:14:07 +0200

    POST and PATCH examples

--- src/test/java/com/framework/java11httpclient/Java11PostFails.java
+++ src/test/java/com/framework/java11httpclient/Java11PostFails.java
@@ -9,8 +9,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static com.framework.java11httpclient.Token.TOKEN;
+
 public class Java11PostFails {
     private static final String BASE_URL = "https://api.github.com/";
+
 
     @Test
     void postWithoutAutorizationFails() throws IOException, InterruptedException {
@@ -28,6 +31,48 @@ public class Java11PostFails {
 
         //Assert
         Assertions.assertEquals(401, actualCode);
+    }
+    @Test
+    void postTest() throws IOException, InterruptedException {
+        String jsonPayload = """
+        {
+            "name": "new-repo",
+            "description": "This is your repository description",
+            "private": false
+        }
+        """;
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest post = HttpRequest.newBuilder(URI.create(BASE_URL+"user/repos"))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + TOKEN)
+                .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
+                .build();
+        HttpResponse<String> response = httpClient.send(post, HttpResponse.BodyHandlers.ofString());
+        System.out.println("response code***: " + response.statusCode());
+        System.out.println("response body***: " + response.body());
+    }
 
+    @Test
+    void patchTest() throws IOException, InterruptedException {
+        String owner = "hmdyyldrm";
+        String repo = "updated-repo2";
+        String jsonPayload = """
+        {
+            "name": "updated-repo",
+            "description": "This is the updated repository description",
+            "private": false
+        }
+        """;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest patchRequest = HttpRequest.newBuilder(URI.create(BASE_URL + "repos/" + owner + "/" + repo))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + TOKEN)
+                .method("PATCH", HttpRequest.BodyPublishers.ofString(jsonPayload))
+                .build();
+
+        HttpResponse<String> response = client.send(patchRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Response body: " + response.body());
     }
 }
---
+++ src/test/java/com/framework/java11httpclient/Token.java
@@ -0,0 +1,5 @@
+package com.framework.java11httpclient;
+
+public class Token {
+    public static final String TOKEN = "***"; // notes selenium token
+}
